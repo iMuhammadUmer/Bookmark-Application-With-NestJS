0eacc8a3becb5a5bcbad6c0c460ae382a8c24c20
Muhammad Umer <mumer1297@gmail.com>
2023-02-17 17:00:02 +0500

    handle duplicate email error

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,28 @@
 import { PrismaService } from '../prisma/prisma.service';
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
   async signup(dto: AuthDto) {
     const hash = await argon.hash(dto.password);
-    const user = await this.prisma.users.create({
-      data: { name: dto.name, email: dto.email, password: hash},
-    });
-    return user;
+    try {
+      const user = await this.prisma.users.create({
+        data: { name: dto.name, email: dto.email, password: hash },
+      });
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Email already registerd.');
+        }
+      } else {
+        throw error;
+      }
+    }
   }
   signin() {
     return 'signed in';
