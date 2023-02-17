17ce84cc3c2895584e980fa0aad418a0ad661323
Muhammad Umer <mumer1297@gmail.com>
2023-02-17 19:04:12 +0500

    add config module to secure DATABASE_URL

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(config: ConfigService) {
     super({
       datasources: {
         db: {
-          url: 'postgresql://postgres:123@localhost:5433/bookmarks_app?schema=public',
+          url: config.get('DATABASE_URL'),
         },
       },
-    });
+    });    
   }
 }
